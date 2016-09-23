57f5222be3cb0d375e149a86a628d04ad7be8383
Andreas Roten <andi@disunconnected.ch>
2016-09-24 01:02:57 +0200

    Winows Forms

---
+++ RI.Framework.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+
+
+
+[assembly: AssemblyTitle("RI.Framework.WinForms")]
+[assembly: AssemblyDescription("RI.Framework.WinForms")]
+[assembly: AssemblyProduct("Utility Framework")]
+[assembly: AssemblyCompany("Roten Informatik")]
+[assembly: AssemblyCopyright("Copyright (c) 2015-2016 Roten Informatik")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
