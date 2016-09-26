cc3357b9a57d70ff9003997b0217621200ebf783
Andreas Roten <andi@disunconnected.ch>
2016-09-26 12:30:13 +0200

    Cleanup

--- RI.Documentation.Framework.Common/PreBuild.bat
+++ RI.Documentation.Framework.Common/PreBuild.bat
@@ -1,3 +1,3 @@
 ﻿@cd %~dp0
 
-@"..\_Input\VersionUpdater.exe" "." "nr" "file" "..\RI.Framework.Property.Version.txt" "..\RI.Framework.Property.Company.txt" "..\RI.Framework.Property.Copyright.txt"
+@"..\_Input\VersionUpdater.exe" "." "r" "file" "..\RI.Framework.Property.Version.txt" "..\RI.Framework.Property.Company.txt" "..\RI.Framework.Property.Copyright.txt"
