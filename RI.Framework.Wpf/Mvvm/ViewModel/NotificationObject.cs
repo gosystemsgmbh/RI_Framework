f3f25f0bf157da458e5a5b7896c1a5d475504d5b
Andreas Roten <andi@disunconnected.ch>
2016-10-04 21:28:12 +0200

    MVVM, Project structure

---
+++ RI.Framework.Wpf/Mvvm/ViewModel/NotificationObject.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+
+
+
+namespace RI.Framework.Mvvm.ViewModel
+{
+	/// <summary>
+	///     Implements a base class which can raise notifications about chaning its state and data.
+	/// </summary>
+	public abstract class NotificationObject : INotifyPropertyChanged
+	{
+		#region Virtuals
+
+		/// <summary>
+		///     Handles the change of all property values by raising the <see cref="PropertyChanged" /> event.
+		/// </summary>
+		protected virtual void OnAllPropertiesChanged ()
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+		}
+
+		/// <summary>
+		///     Handles the change of a property value by raising the <see cref="PropertyChanged" /> event.
+		/// </summary>
+		/// <param name="propertyName"> The name of the property which has changed. </param>
+		protected virtual void OnPropertyChanged (string propertyName)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+
+
+
+
+		#region Interface: INotifyPropertyChanged
+
+		/// <inheritdoc />
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+	}
+}
--- RI.Framework.Wpf/Mvvm/ViewModel/ViewModelBase.cs
+++ RI.Framework.Wpf/Mvvm/ViewModel/ViewModelBase.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-
-using RI.Framework.Services.Regions;
+﻿using RI.Framework.Services.Regions;
 
 
 
@@ -10,36 +8,12 @@ namespace RI.Framework.Mvvm.ViewModel
 	/// <summary>
 	///     Implements a base class for view models.
 	/// </summary>
-	public abstract class ViewModelBase : IViewModel, IRegionElement
+	public abstract class ViewModelBase : NotificationObject, IViewModel, IRegionElement
 	{
-		#region Instance Properties/Indexer
+		#region Interface: IRegionElement
 
-		/// <summary>
-		///     Gets or sets whether the view is initialized or not.
-		/// </summary>
-		/// <value>
-		///     true if the view is initialized, false otherwise.
-		/// </value>
-		protected bool IsInitialized { get; set; }
-
-		#endregion
-
-
-
-
-		#region Instance Events
-
-		private event PropertyChangedEventHandler PropertyChanged;
-
-		#endregion
-
-
-
-
-		#region Virtuals
-
-		/// <inheritdoc cref="IRegionElement.SortIndex" />
-		protected virtual int? SortIndex
+		/// <inheritdoc />
+		public virtual int? SortIndex
 		{
 			get
 			{
@@ -47,110 +21,36 @@ namespace RI.Framework.Mvvm.ViewModel
 			}
 		}
 
-		/// <inheritdoc cref="IRegionElement.Activated" />
-		protected virtual void Activated ()
+		/// <inheritdoc />
+		public virtual void Activated ()
 		{
 		}
 
-		/// <inheritdoc cref="IRegionElement.CanNavigateFrom" />
-		protected virtual bool CanNavigateFrom ()
+		/// <inheritdoc />
+		public virtual bool CanNavigateFrom ()
 		{
 			return true;
 		}
 
-		/// <inheritdoc cref="IRegionElement.CanNavigateTo" />
-		protected virtual bool CanNavigateTo ()
+		/// <inheritdoc />
+		public virtual bool CanNavigateTo ()
 		{
 			return true;
 		}
 
-		/// <inheritdoc cref="IRegionElement.Deactivated" />
-		protected virtual void Deactivated ()
-		{
-		}
-
-		/// <inheritdoc cref="IViewModel.Initialize" />
-		protected virtual void Initialize ()
-		{
-		}
-
-		/// <inheritdoc cref="IRegionElement.NavigatedFrom" />
-		protected virtual void NavigatedFrom ()
-		{
-		}
-
-		/// <inheritdoc cref="IRegionElement.NavigatedTo" />
-		protected virtual void NavigatedTo ()
-		{
-		}
-
-		/// <summary>
-		///     Handles the change of all property values by raising the <see cref="PropertyChanged" /> event.
-		/// </summary>
-		protected virtual void OnAllPropertiesChanged ()
-		{
-			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
-		}
-
-		/// <summary>
-		///     Handles the change of a property value by raising the <see cref="PropertyChanged" /> event.
-		/// </summary>
-		/// <param name="propertyName"> The name of the property which has changed. </param>
-		protected virtual void OnPropertyChanged (string propertyName)
-		{
-			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-
-		#endregion
-
-
-
-
-		#region Interface: IRegionElement
-
 		/// <inheritdoc />
-		int? IRegionElement.SortIndex
+		public virtual void Deactivated ()
 		{
-			get
-			{
-				return this.SortIndex;
-			}
 		}
 
 		/// <inheritdoc />
-		void IRegionElement.Activated ()
+		public virtual void NavigatedFrom ()
 		{
-			this.Activated();
 		}
 
 		/// <inheritdoc />
-		bool IRegionElement.CanNavigateFrom ()
+		public virtual void NavigatedTo ()
 		{
-			return this.CanNavigateFrom();
-		}
-
-		/// <inheritdoc />
-		bool IRegionElement.CanNavigateTo ()
-		{
-			return this.CanNavigateTo();
-		}
-
-		/// <inheritdoc />
-		void IRegionElement.Deactivated ()
-		{
-			this.Deactivated();
-		}
-
-		/// <inheritdoc />
-		void IRegionElement.NavigatedFrom ()
-		{
-			this.NavigatedFrom();
-		}
-
-		/// <inheritdoc />
-		void IRegionElement.NavigatedTo ()
-		{
-			this.NavigatedTo();
 		}
 
 		#endregion
@@ -161,31 +61,11 @@ namespace RI.Framework.Mvvm.ViewModel
 		#region Interface: IViewModel
 
 		/// <inheritdoc />
-		bool IViewModel.IsInitialized
-		{
-			get
-			{
-				return this.IsInitialized;
-			}
-		}
+		public bool IsInitialized { get; protected set; }
 
 		/// <inheritdoc />
-		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+		public virtual void Initialize ()
 		{
-			add
-			{
-				this.PropertyChanged += value;
-			}
-			remove
-			{
-				this.PropertyChanged -= value;
-			}
-		}
-
-		/// <inheritdoc />
-		void IViewModel.Initialize ()
-		{
-			this.Initialize();
 		}
 
 		#endregion
