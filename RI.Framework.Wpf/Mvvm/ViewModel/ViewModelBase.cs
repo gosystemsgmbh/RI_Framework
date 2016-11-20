12bc7de9eb79598ca0624579ffddaddebb8b01a8
Andreas Roten <andi@disunconnected.ch>
2016-11-20 16:00:51 +0100

    MVVM

--- RI.Framework.Wpf/Mvvm/ViewModel/ViewModelBase.cs
+++ RI.Framework.Wpf/Mvvm/ViewModel/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using RI.Framework.Services.Regions;
+﻿using RI.Framework.Services.Logging;
+using RI.Framework.Services.Regions;
 
 
 
@@ -10,6 +11,42 @@ namespace RI.Framework.Mvvm.ViewModel
 	/// </summary>
 	public abstract class ViewModelBase : NotificationObject, IViewModel, IRegionElement
 	{
+		#region Instance Methods
+
+		/// <summary>
+		///     Logs a message.
+		/// </summary>
+		/// <param name="severity"> The severity of the message. </param>
+		/// <param name="format"> The message. </param>
+		/// <param name="args"> The arguments which will be expanded into the message (comparable to <see cref="string.Format(string, object[])" />). </param>
+		/// <remarks>
+		///     <para>
+		///         <see cref="ILogService" /> is used, obtained through <see cref="LogLocator" />.
+		///         If no <see cref="ILogService" /> is available, no logging is performed.
+		///     </para>
+		/// </remarks>
+		protected void Log (LogLevel severity, string format, params object[] args)
+		{
+			LogLocator.Log(severity, this.GetType().Name, format, args);
+		}
+
+		#endregion
+
+
+
+
+		#region Virtuals
+
+		/// <inheritdoc cref="IViewModel.Initialize" />
+		protected virtual void Initialize ()
+		{
+		}
+
+		#endregion
+
+
+
+
 		#region Interface: IRegionElement
 
 		/// <inheritdoc />
@@ -61,11 +98,15 @@ namespace RI.Framework.Mvvm.ViewModel
 		#region Interface: IViewModel
 
 		/// <inheritdoc />
-		public bool IsInitialized { get; protected set; }
+		public bool IsInitialized { get; private set; }
 
 		/// <inheritdoc />
-		public virtual void Initialize ()
+		void IViewModel.Initialize ()
 		{
+			this.Log(LogLevel.Debug, "Initializing module");
+
+			this.Initialize();
+
 			this.IsInitialized = true;
 		}
 
