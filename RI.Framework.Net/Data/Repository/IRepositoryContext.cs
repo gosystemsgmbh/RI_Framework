a7d8037ba6bd4393001aca4f2262174b705bcecf
Roten Informatik <RotenInformatik@users.noreply.github.com>
2016-11-30 10:20:06 +0100

    Update IRepositoryContext.cs

--- RI.Framework.Net/Data/Repository/IRepositoryContext.cs
+++ RI.Framework.Net/Data/Repository/IRepositoryContext.cs
@@ -21,7 +21,6 @@ namespace RI.Framework.Data.Repository
 	///         Repository contexts are intended to be short-living instances which are created when necessary and disposed after the data-specific tasks (&quot;unit of work&quot;) are completed.
 	///     </para>
 	/// </remarks>
-	/// TODO: Add non-generic version of GetSet
 	public interface IRepositoryContext : IDisposable
 	{
 		/// <summary>
@@ -31,7 +30,19 @@ namespace RI.Framework.Data.Repository
 		void Commit ();
 
 		/// <summary>
-		///     Gets the repository set for a certain type of entities.
+		///     Gets the non-generic repository set for a certain type of entities.
+		/// </summary>
+		/// <param name="type"> The type of entities. </param>
+		/// <returns>
+		///     The repository set for the entities of type <paramref name="type" />.
+		/// </returns>
+		/// <exception cref="ObjectDisposedException"> The repository context is disposed. </exception>
+		/// <exception cref="ArgumentNullException"> <paramref name="type" /> is null. </exception>
+		/// <exception cref="InvalidTypeArgumentException"> The type <paramref name="type" /> does not represent a valid entity type for which a repository set can be retrieved. </exception>
+		IRepositorySet GetSet (Type type);
+
+		/// <summary>
+		///     Gets the generic repository set for a certain type of entities.
 		/// </summary>
 		/// <typeparam name="T"> The type of entities. </typeparam>
 		/// <returns>
