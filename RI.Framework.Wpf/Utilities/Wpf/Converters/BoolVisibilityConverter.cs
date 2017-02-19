3742545256e93ba7c34ba72cef78fa06f0c39b09
Andreas Roten <andi@disunconnected.ch>
2017-02-19 18:19:21 +0100

    WPF

---
+++ RI.Framework.Wpf/Utilities/Wpf/Converters/BoolVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+
+
+
+namespace RI.Framework.Utilities.Wpf.Converters
+{
+	[ValueConversion(typeof(bool), typeof(Visibility), ParameterType = typeof(Visibility))]
+	public sealed class BoolVisibilityConverter : IValueConverter
+	{
+		/// <inheritdoc />
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return ((bool)value) ? Visibility.Visible : (Visibility)parameter;
+		}
+
+		/// <inheritdoc />
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new InvalidOperationException(this.GetType().Name + " cannot convert back a value to the source.");
+		}
+	}
+}
---
+++ RI.Framework.Wpf/Utilities/Wpf/Converters/ObjectVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+
+
+
+namespace RI.Framework.Utilities.Wpf.Converters
+{
+	[ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(Visibility))]
+	public sealed class ObjectVisibilityConverter : IValueConverter
+	{
+		/// <inheritdoc />
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (value != null) ? Visibility.Visible : (Visibility)parameter;
+		}
+
+		/// <inheritdoc />
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new InvalidOperationException(this.GetType().Name + " cannot convert back a value to the source.");
+		}
+	}
+}
