f0d82051b75674ef47435b09f5e6c372cc533b5f
Andreas Roten <andi@disunconnected.ch>
2017-05-16 22:44:01 +0200

    Documentation

--- RI.Documentation.Framework.Unity.Light/PostBuild.bat
+++ RI.Documentation.Framework.Unity.Light/PostBuild.bat
@@ -5,7 +5,7 @@
 @copy ".\Help\RI.Documentation.Framework.Unity.Light.docx" "..\_Output\RI.Documentation.Framework.Unity.Light.docx"
 @copy ".\Help\RI.Documentation.Framework.Unity.Light.docx" "..\_Output\DecouplingUtilities.docx"
 
-@cscript "..\_Input\Doc2Pdf.vbs" ".\Help\RI.Documentation.Framework.Unity.Light.docx"
+@cscript "..\_Input\ConvDoc2Pdf.vbs" ".\Help\RI.Documentation.Framework.Unity.Light.docx"
 
 @copy ".\Help\RI.Documentation.Framework.Unity.Light.pdf" "..\_Output\RI.Documentation.Framework.Unity.Light.pdf"
 @copy ".\Help\RI.Documentation.Framework.Unity.Light.pdf" "..\_Output\DecouplingUtilities.pdf"
--- RI.Documentation.Framework.Unity.Light/PreBuild.bat
+++ RI.Documentation.Framework.Unity.Light/PreBuild.bat
@@ -2,15 +2,20 @@
 
 @"..\_Input\VersionUpdater.exe" "." "nr" "file" "..\RI.Framework.Property.Version.txt" "..\RI.Framework.Property.Company.txt" "..\RI.Framework.Property.Copyright.txt"
 
-@xcopy "..\RI.Documentation.Framework.Unity\Content\Introduction.aml" ".\Content\Introduction.aml" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Content\Compatibility.aml" ".\Content\Compatibility.aml" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Content\Usage.aml" ".\Content\Usage.aml" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Content\ContactSupport.aml" ".\Content\ContactSupport.aml" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Content\VersionHistory.aml" ".\Content\VersionHistory.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\Introduction.aml" ".\Content\Introduction.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\Compatibility.aml" ".\Content\Compatibility.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\Usage.aml" ".\Content\Usage.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\ContactSupport.aml" ".\Content\ContactSupport.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\VersionHistory.aml" ".\Content\VersionHistory.aml" /E /Y
 
-@xcopy "..\RI.Documentation.Framework.Unity\Content\OverviewTutorials\xxx.aml" ".\Content\OverviewTutorials\xxx.aml" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Content\OverviewTutorials\xxx.aml" ".\Content\OverviewTutorials\xxx.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\OverviewTutorials.aml" ".\Content\OverviewTutorials\OverviewTutorials.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\Bootstrapper.aml" ".\Content\OverviewTutorials\Bootstrapper.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\CompositionContainer.aml" ".\Content\OverviewTutorials\CompositionContainer.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\ServiceLocator.aml" ".\Content\OverviewTutorials\ServiceLocator.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\ModuleService.aml" ".\Content\OverviewTutorials\ModuleService.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\DispatcherService.aml" ".\Content\OverviewTutorials\DispatcherService.aml" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Content\OverviewTutorials\ExampleMods.aml" ".\Content\OverviewTutorials\ExampleMods.aml" /E /Y
 
-@xcopy "..\RI.Documentation.Framework.Unity\Media\BootstrapperDragDrop.png" ".\Media\BootstrapperDragDrop.png" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Media\BootstrapperObject.png" ".\Media\BootstrapperObject.png" /E /Y
-@xcopy "..\RI.Documentation.Framework.Unity\Media\BootstrapperOptions.png" ".\Media\BootstrapperOptions.png" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Media\BootstrapperDragDrop.png" ".\Media\BootstrapperDragDrop.png" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Media\BootstrapperObject.png" ".\Media\BootstrapperObject.png" /E /Y
+@xcopy "..\RI.Documentation.Framework.Unity.Heavy\Media\BootstrapperOptions.png" ".\Media\BootstrapperOptions.png" /E /Y
