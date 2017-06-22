8a3859338b6c6bb9d2bf3baad73ad907223a6ba5
Andreas Roten <andi@disunconnected.ch>
2017-06-22 10:40:40 +0200

    Streams, EventLogWriter

---
+++ RI.Framework.Common/IO/Streams/SynchronizedStream.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.IO;
+
+using RI.Framework.Utilities.ObjectModel;
+
+namespace RI.Framework.IO.Streams
+{
+	/// <summary>
+	/// Implements a stream which wraps another stream and synchronizes acces to it using a synchronization object.
+	/// </summary>
+	/// <remarks>
+	/// <value>
+	/// <see cref="SynchronizedStream"/> can be used to synchronize access to a stream from multiple threads.
+	/// </value>
+	/// <note type="important">
+	/// Only the access to <see cref="Stream"/> members are synchronized.
+	/// Repeated access (e.g. a <see cref="Seek"/> followed by a <see cref="Write"/>), which rely on to be of atomic nature, must be synchronized on a higher level, by the users of <see cref="SynchronizedStream"/>, using <see cref="SyncRoot"/>.
+	/// </note>
+	/// </remarks>
+	public sealed class SynchronizedStream : Stream, ISynchronizable
+	{
+		/// <summary>
+		/// Creates a new instance of <see cref="SynchronizedStream"/>.
+		/// </summary>
+		/// <param name="stream">The stream to wrap.</param>
+		/// <remarks>
+		/// <para>
+		/// A new, dedicated synchronization object is created and used.
+		/// </para>
+		/// </remarks>
+		/// <exception cref="ArgumentNullException"><paramref name="stream"/> is null.</exception>
+		public SynchronizedStream(Stream stream)
+			: this(stream, null)
+		{
+		}
+
+		/// <summary>
+		/// Creates a new instance of <see cref="SynchronizedStream"/>.
+		/// </summary>
+		/// <param name="stream">The stream to wrap.</param>
+		/// <param name="syncRoot">The synchronization object to use. Can be null to create a new, dedicated synchronization object.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="stream"/> is null.</exception>
+		public SynchronizedStream(Stream stream, object syncRoot)
+		{
+			if (stream == null)
+			{
+				throw new ArgumentNullException(nameof(stream));
+			}
+
+			this.BaseStream = stream;
+			this.SyncRoot = syncRoot ?? new object();
+		}
+
+		/// <summary>
+		///     Garbage collects this instance of <see cref="SynchronizedStream" />.
+		/// </summary>
+		~SynchronizedStream()
+		{
+			this.Close();
+		}
+
+		/// <summary>
+		/// Gets the wrapped stream.
+		/// </summary>
+		/// <value>
+		/// The wrapped stream.
+		/// </value>
+		public Stream BaseStream { get; }
+
+		/// <inheritdoc />
+		bool ISynchronizable.IsSynchronized => true;
+
+		/// <inheritdoc />
+		public object SyncRoot { get; }
+
+		/// <inheritdoc />
+		public override void Flush ()
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.Flush();
+			}
+		}
+
+		/// <inheritdoc />
+		public override int Read (byte[] buffer, int offset, int count)
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.Read(buffer, offset, count);
+			}
+		}
+
+		/// <inheritdoc />
+		public override long Seek (long offset, SeekOrigin origin)
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.Seek(offset, origin);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void SetLength (long value)
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.SetLength(value);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void Write (byte[] buffer, int offset, int count)
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.Write(buffer, offset, count);
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool CanRead
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.CanRead;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool CanSeek
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.CanSeek;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool CanWrite
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.CanWrite;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override long Length
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.Length;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override long Position
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.Position;
+				}
+			}
+			set
+			{
+				lock (this.SyncRoot)
+				{
+					this.BaseStream.Position = value;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		protected override void Dispose (bool disposing)
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.Close();
+				base.Dispose(disposing);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void Close ()
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.Close();
+				base.Close();
+			}
+		}
+
+		/// <inheritdoc />
+		public override IAsyncResult BeginRead (byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.BeginRead(buffer, offset, count, callback, state);
+			}
+		}
+
+		/// <inheritdoc />
+		public override IAsyncResult BeginWrite (byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.BeginWrite(buffer, offset, count, callback, state);
+			}
+		}
+
+		/// <inheritdoc />
+		public override int EndRead (IAsyncResult asyncResult)
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.EndRead(asyncResult);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void EndWrite (IAsyncResult asyncResult)
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.EndWrite(asyncResult);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void WriteByte (byte value)
+		{
+			lock (this.SyncRoot)
+			{
+				this.BaseStream.WriteByte(value);
+			}
+		}
+
+		/// <inheritdoc />
+		public override int ReadByte ()
+		{
+			lock (this.SyncRoot)
+			{
+				return this.BaseStream.ReadByte();
+			}
+		}
+
+		/// <inheritdoc />
+		public override int WriteTimeout
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.WriteTimeout;
+				}
+			}
+			set
+			{
+				lock (this.SyncRoot)
+				{
+					this.BaseStream.WriteTimeout = value;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override int ReadTimeout
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.ReadTimeout;
+				}
+			}
+			set
+			{
+				lock (this.SyncRoot)
+				{
+					this.BaseStream.ReadTimeout = value;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool CanTimeout
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this.BaseStream.CanTimeout;
+				}
+			}
+		}
+	}
+}
---
+++ RI.Framework.Windows.Shared/Services/Logging/Writers/EventLogWriter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+using RI.Framework.Composition.Model;
+using RI.Framework.Mathematic;
+using RI.Framework.Utilities;
+using RI.Framework.Utilities.ObjectModel;
+
+namespace RI.Framework.Services.Logging.Writers
+{
+	/// <summary>
+	///     Implements a log writer which writes to <see cref="System.Diagnostics.EventLog"/>.
+	/// </summary>
+	/// <remarks>
+	///     <para>
+	///         <see cref="System.Diagnostics.EventLog.WriteEntry(string,EventLogEntryType)" /> is used to write the log messages.
+	///     </para>
+	///     <para>
+	///         See <see cref="ILogWriter" /> for more details.
+	///     </para>
+	/// </remarks>
+	[Export]
+	public sealed class EventLogWriter : ILogWriter, ILogSource
+	{
+		/// <summary>
+		/// Gets the used event log.
+		/// </summary>
+		/// <value>
+		/// The used event log.
+		/// </value>
+		public EventLog EventLog { get; }
+
+		/// <summary>
+		///     Creates a new instance of <see cref="EventLogWriter" />.
+		/// </summary>
+		/// <param name="eventLog">The event log to use.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="eventLog"/> is null.</exception>
+		public EventLogWriter(EventLog eventLog)
+		{
+			if (eventLog == null)
+			{
+				throw new ArgumentNullException(nameof(eventLog));
+			}
+
+			this.SyncRoot = new object();
+
+			this.EventLog = eventLog;
+		}
+
+
+
+		#region Instance Fields
+
+		private ILogFilter _filter;
+
+		#endregion
+
+
+
+
+		#region Instance Properties/Indexer
+
+		private object SyncRoot { get; set; }
+
+		#endregion
+
+
+
+
+		#region Interface: ILogWriter
+
+		/// <inheritdoc />
+		public ILogFilter Filter
+		{
+			get
+			{
+				lock (this.SyncRoot)
+				{
+					return this._filter;
+				}
+			}
+			set
+			{
+				lock (this.SyncRoot)
+				{
+					this._filter = value;
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		bool ISynchronizable.IsSynchronized => true;
+
+		/// <inheritdoc />
+		object ISynchronizable.SyncRoot => this.SyncRoot;
+
+		/// <inheritdoc />
+		public void Cleanup(DateTime retentionDate)
+		{
+			lock (this.SyncRoot)
+			{
+				int retentionDays = (int)DateTime.Now.Date.Subtract(retentionDate.Date).TotalDays + 1;
+				retentionDays = retentionDays.Clamp(1, 365);
+
+				try
+				{
+					this.EventLog.ModifyOverflowPolicy(OverflowAction.OverwriteOlder, retentionDays);
+				}
+				catch (InvalidOperationException exception)
+				{
+					this.Log(LogLevel.Warning, "Could not cleanup event log: {0}", exception.ToDetailedString());
+				}
+			}
+		}
+
+		/// <inheritdoc />
+		public void Log(DateTime timestamp, int threadId, LogLevel severity, string source, string message)
+		{
+			if (this.Filter != null)
+			{
+				if (!this.Filter.Filter(timestamp, threadId, severity, source))
+				{
+					return;
+				}
+			}
+
+			lock (this.SyncRoot)
+			{
+				source = source ?? "null";
+				message = message ?? string.Empty;
+
+				StringBuilder finalMessageBuilder = new StringBuilder();
+				finalMessageBuilder.Append("[");
+				finalMessageBuilder.Append(timestamp.ToSortableString());
+				finalMessageBuilder.Append("] [");
+				finalMessageBuilder.Append(threadId.ToString("D4", CultureInfo.InvariantCulture));
+				finalMessageBuilder.Append("] [");
+				finalMessageBuilder.Append(severity.ToString()[0]);
+				finalMessageBuilder.Append("] [");
+				finalMessageBuilder.Append(source);
+				finalMessageBuilder.Append("] ");
+				finalMessageBuilder.AppendLine(message);
+				string finalMessage = finalMessageBuilder.ToString();
+
+				EventLogEntryType entryType = EventLogEntryType.Information;
+				if (severity == LogLevel.Warning)
+				{
+					entryType = EventLogEntryType.Warning;
+				}
+				else if ((severity == LogLevel.Error) || (severity == LogLevel.Fatal))
+				{
+					entryType = EventLogEntryType.Error;
+				}
+
+				try
+				{
+					this.EventLog.WriteEntry(finalMessage, entryType);
+				}
+				catch
+				{
+				}
+			}
+		}
+
+		#endregion
+	}
+}
