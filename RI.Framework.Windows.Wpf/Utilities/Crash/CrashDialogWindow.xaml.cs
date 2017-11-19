d7ea5644009014b60e160220b731416d1c3f76b6
Andreas Roten <andi@disunconnected.ch>
2017-11-19 17:15:57 +0100

    Moved crash handling types

---
+++ RI.Framework.Windows.Wpf/Utilities/Crash/CrashDialogWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+
+
+
+
+namespace RI.Framework.Utilities.Crash
+{
+	internal partial class CrashDialogWindow : Window
+	{
+		#region Instance Constructor/Destructor
+
+		public CrashDialogWindow ()
+		{
+			this.InitializeComponent();
+		}
+
+		#endregion
+
+
+
+
+		#region Instance Events
+
+		public event EventHandler ShowReportDetails;
+
+		#endregion
+
+
+
+
+		#region Instance Methods
+
+		private void Button_OnClick (object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void ReportDetails_OnClick (object sender, RoutedEventArgs e)
+		{
+			this.ShowReportDetails?.Invoke(this, EventArgs.Empty);
+		}
+
+		#endregion
+	}
+}
