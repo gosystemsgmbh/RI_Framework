a9c4db13c2f5279f7e856c7c0e62e57f25a3c046
Andreas Roten <andi@disunconnected.ch>
2017-11-19 17:17:55 +0100

    Removed projects not for this solution

--- RI.Framework.RotenInformatik.Shared/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-
-
-
-[assembly: AssemblyTitle("RI.Framework.RotenInformatik.Shared")]
-[assembly: AssemblyDescription("RI.Framework.RotenInformatik.Shared")]
-[assembly: Guid("842850D7-3F53-49E0-8830-D2BF95CBBCD5")]
