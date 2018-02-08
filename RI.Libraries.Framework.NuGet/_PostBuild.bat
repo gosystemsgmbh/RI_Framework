41c891777dcf0540782865b58847f9818d526c5d
Andreas Roten <andi@disunconnected.ch>
2018-02-08 22:07:09 +0100

    #15 Updated several NuGet packages

--- RI.Libraries.Framework.NuGet/_PostBuild.bat
+++ RI.Libraries.Framework.NuGet/_PostBuild.bat
@@ -2,17 +2,17 @@
 
 @copy "..\packages\BCrypt.Net-Next.2.1.2\lib\net452\*.dll" "..\_Output\*.*"
 
-@copy "..\packages\ControlzEx.2.2.0.4\lib\net45\*.dll" "..\_Output\*.*"
+@copy "..\packages\ControlzEx.3.0.2.4\lib\net45\*.dll" "..\_Output\*.*"
 
 @copy "..\packages\DotNetZip.1.10.1\lib\net20\*.dll" "..\_Output\*.*"
 
-@copy "..\packages\EntityFramework.6.1.3\lib\net45\*.dll" "..\_Output\*.*"
+@copy "..\packages\EntityFramework.6.2.0\lib\net45\*.dll" "..\_Output\*.*"
 
 @copy "..\packages\EPPlus.4.1.0\lib\net40\*.dll" "..\_Output\*.*"
 
 @copy "..\packages\Extended.Wpf.Toolkit.3.2.0\lib\net40\*.dll" "..\_Output\*.*"
 
-@copy "..\packages\Fluent.Ribbon.5.0.2.46\lib\net45\*.dll" "..\_Output\*.*"
+@copy "..\packages\Fluent.Ribbon.6.0.0.208\lib\net45\*.dll" "..\_Output\*.*"
 
 @copy "..\packages\Microsoft.AspNet.Razor.3.2.3\lib\net45\*.dll" "..\_Output\*.*"
 
@@ -30,8 +30,8 @@
 
 @copy "..\packages\RestSharp.106.1.0\lib\net452\*.dll" "..\_Output"
 
-@copy "..\packages\System.Data.SQLite.Core.1.0.106.0\lib\net46\*.dll" "..\_Output\*.*"
-@copy "..\packages\System.Data.SQLite.Core.1.0.106.0\build\net46\x86\*.dll" "..\_Output\x86\*.*"
-@copy "..\packages\System.Data.SQLite.Core.1.0.106.0\build\net46\x64\*.dll" "..\_Output\x64\*.*"
-@copy "..\packages\System.Data.SQLite.EF6.1.0.106.0\lib\net46\*.dll" "..\_Output\*.*"
-@copy "..\packages\System.Data.SQLite.Linq.1.0.106.0\lib\net46\*.dll" "..\_Output\*.*"
+@copy "..\packages\System.Data.SQLite.Core.1.0.107.0\lib\net46\*.dll" "..\_Output\*.*"
+@copy "..\packages\System.Data.SQLite.Core.1.0.107.0\build\net46\x86\*.dll" "..\_Output\x86\*.*"
+@copy "..\packages\System.Data.SQLite.Core.1.0.107.0\build\net46\x64\*.dll" "..\_Output\x64\*.*"
+@copy "..\packages\System.Data.SQLite.EF6.1.0.107.0\lib\net46\*.dll" "..\_Output\*.*"
+@copy "..\packages\System.Data.SQLite.Linq.1.0.107.0\lib\net46\*.dll" "..\_Output\*.*"
