04c13d17697edd8a1d5831e4226c61bf4b56765f
Andreas Roten <andi@disunconnected.ch>
2018-10-13 11:10:48 +0200

    #80 Renamed Unity project #87 Moved types to RI.Framework.Common #93 Removed unused projects

--- RI.Framework.Unity.Modern/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-
-
-
-[assembly: AssemblyTitle("RI.Framework.Unity.Modern")]
-[assembly: AssemblyDescription("RI.Framework.Unity.Modern")]
-[assembly: Guid("A7D6EF09-3DDD-4CD7-85CB-9E117BE70955")]
--- RI.Framework.Unity.Modern/_PostBuild.bat
+++
@@ -1,3 +0,0 @@
-@cd %~dp0
-
-@copy ".\bin\%1\netstandard2.0\RI.Framework.Unity.Modern.*" "..\_Output\*.*"
