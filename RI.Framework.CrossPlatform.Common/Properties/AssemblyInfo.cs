572a2ba01c1fbea8347ddc38546406e75f8c74ed
Andreas Roten <andi@disunconnected.ch>
2019-03-14 19:55:40 +0100

    #81 Removed RI.Framework.CrossPlatform.Common #22 Reviews

--- RI.Framework.CrossPlatform.Common/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-
-
-
-[assembly: AssemblyTitle("RI.Framework.CrossPlatform.Common")]
-[assembly: AssemblyDescription("RI.Framework.CrossPlatform.Common")]
-[assembly: Guid("26F32EFE-58D1-4F18-B454-465D2805835A")]
--- RI.Tools.Framework.TemplateRunner/_PostBuild.bat
+++
@@ -1,4 +0,0 @@
-@cd %~dp0
-
-@copy ".\NumberExtensions.tt.cs" "..\RI.Framework.Common\Mathematic\NumberExtensions.cs"
-@copy ".\MathUtils.tt.cs" "..\RI.Framework.Common\Mathematic\MathUtils.cs"
