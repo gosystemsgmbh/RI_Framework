dce3e17f6e8e99f9c81c1878bd37caedf90d91e8
Andreas Roten <andi@disunconnected.ch>
2019-06-10 00:11:02 +0200

    #68 Updated SQLite NuGet packages

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -10,9 +10,9 @@ using System.Reflection;
 [assembly: AssemblyTrademark("Licensed under the Apache License 2.0")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("0.7.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
